1276aea777f578fd3460866495b3dcaa35d803cb
baby3hjjhzv <nawolak68@gmail.com>
2024-07-22 09:08:24 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,11 +3,11 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.ownernumber = [ process.env.OWNER_NUMBER || "94715898396"],
+global.ownername = process.env.OWNER_NAME || "💙𓆩‎DARK-HAKER𓆪_✰",
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "Sri-lanka"
 
 global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
 
